98c4e251875b7452178cd297c0678eb0d57a249b
uNastyy_ <bouissouromain82@gmail.com>
2024-03-12 10:20:59 +0100

    enregistrer

---
+++ enregistrer.php
@@ -0,0 +1,25 @@
+<?php
+include_once("BD.php");
+$BD = new BD();
+
+$pseudo = $_POST['pseudo'];
+$phrase = $_POST['phrase'];
+
+$timestamp = time();
+
+function enregistrer($pseudo, $phrase, $timestamp) {
+    global $BD;
+    try {
+        $sql = "INSERT INTO chatJS (contenu, userPseudo, horaire) VALUES (:contenu, :userPseudo, :horaire)";
+        $stmt = $BD->getBD()->prepare($sql);
+
+        $stmt->bindParam(':contenu', $pseudo);
+        $stmt->bindParam(':userPseudo', $phrase);
+        $stmt->bindParam(':horaire', $timestamp);
+
+        $stmt->execute();
+
+    } catch (PDOException $e) {
+        die("Erreur d'insertion dans la base de données: " . $e->getMessage());
+    }
+}
