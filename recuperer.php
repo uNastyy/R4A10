db2fb53dbb8051a0538421437571dad6a0fe032d
Yeetnus <yanis.croguennoc@etu.iut-tlse3.Fr>
2024-03-12 10:20:55 +0100

    update recuperer.php

--- recuperer.php
+++ recuperer.php
@@ -1,5 +1,11 @@
 <?php
       require("BD.php");
       $BD = new BD();
-      function 
+      function getLast10($table, $id)
+      {
+          global $BD;
+          $req = $BD->getBD()->prepare("SELECT * FROM chatjs where idMessage>(select count(idMessage)-10 from chatjs)");
+          $req->execute();
+          return $req->fetch();
+      }
       ?>
