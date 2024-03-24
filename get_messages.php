bdc9f74ebfb6c58a3ce5a5b02580dfd5846d9f17
uNastyy_ <bouissouromain82@gmail.com>
2024-03-24 14:14:23 +0100

    faut fix les 10 premiers messages y'a tout qui s'affiche là mais flm

--- get_messages.php
+++ get_messages.php
@@ -2,17 +2,19 @@
 require("BD.php");
 $BD = new BD();
 
-function getLastMessage()
+function getMessages()
 {
     global $BD;
-    $req = $BD->getBD()->prepare("SELECT idMessage, contenu, userPseudo, DATE_FORMAT(FROM_UNIXTIME(horaire), '%d/%m/%Y %H:%i:%s') as horaire FROM chatJS ORDER BY idMessage DESC LIMIT 1");
+    $req = $BD->getBD()->prepare("SELECT idMessage, contenu, userPseudo, DATE_FORMAT(FROM_UNIXTIME(horaire), '%d/%m/%Y %H:%i:%s') as horaire FROM chatJS ORDER BY idMessage ASC");
     $req->execute();
-    return $req->fetch();
+    return $req->fetchAll();
 }
-$result = getLastMessage();
+$results = getMessages();
 
-echo '<div class="message">';
-echo '<p class="contenu">' . htmlspecialchars($result["contenu"]) . '</p>';
-echo '<span class="userPseudo">' . htmlspecialchars($result["userPseudo"]) . '</span> | ';
-echo '<span class="horaire">' . htmlspecialchars($result["horaire"]) . '</span>';
-echo '</div>';
+foreach ($results as $result) {
+    echo '<div class="message">';
+    echo '<p class="contenu">' . htmlspecialchars($result["contenu"]) . '</p>';
+    echo '<span class="userPseudo">' . htmlspecialchars($result["userPseudo"]) . '</span> | ';
+    echo '<span class="horaire">' . htmlspecialchars($result["horaire"]) . '</span>';
+    echo '</div>';
+}
